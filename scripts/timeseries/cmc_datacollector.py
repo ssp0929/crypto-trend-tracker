1489403201717e283b885b2961deefa841ed8650
Stephen Park <ssp0929@gmail.com>
2017-12-28 17:15:06 -0600

    Lint warnings/errors cleaned up.

--- scripts/timeseries/cmc_datacollector.py
+++ scripts/timeseries/cmc_datacollector.py
@@ -1,28 +1,31 @@
+'''
+Simple time-series data collector to establish a dataframe of
+cryptocurrency prices over discrete time points. The crypto-
+currencies to analyze will be targeted from a list given in a .txt
+file. Initial assessment is to be conducted hourly over a monthly
+time period. Results are output into a CSV file to later analyze
+'''
+
 import time
 
 import coinmarketcap
 import pandas as pd
 import numpy as np
 
-#######################################################################
-#######################################################################
-# Simple time-series data collector to establish a dataframe of       #
-# cryptocurrency prices over discrete time points. The crypto-        #
-# currencies to analyze will be targeted from a list given in a .txt  #
-# file. Initial assessment is to be conducted hourly over a monthly   #
-# time period. Results are output into a CSV file to later analyze    #
-#######################################################################
-#######################################################################
 
-# function to return price of a cryptocurrency from cmc api
 def get_price(currency, market):
+
+    '''function to return price of a cryptocurrency from cmc api'''
+
     crypto = market.ticker(currency)[0]
     crypto_price = float(crypto.get('price_usd'))
 
     return crypto_price
 
-# create list of cryptos from file containing targets
 def gen_list():
+
+    '''create list of cryptos from file containing targets'''
+
     c_list = []
     with open("crypto_list.txt", "r") as readfile:
         for line in readfile:
@@ -34,6 +37,12 @@ def gen_list():
 # update dataframe entries (i,j) by iterating through
 # and retrieving data from api. Look at upgrading this method.
 def update_dataframe(crypto_array, crypto_list, discrete_points, market):
+
+    '''
+    update dataframe entries (i,j) by iterating through
+    and retrieving data from api. Look at upgrading this method.
+    '''
+
     for row in range(discrete_points):
         for currency in crypto_list:
             price = get_price(currency, market)
@@ -43,13 +52,19 @@ def update_dataframe(crypto_array, crypto_list, discrete_points, market):
         time.sleep(3600)
 
 def main():
+
+    ''' program driver '''
+
     # initialize parameters
     market = coinmarketcap.Market()
     discrete_points = 720
     crypto_list = gen_list()
 
     # memory/efficieny problems
-    crypto_array = pd.DataFrame(index=np.arange(0, discrete_points), columns=(i for i in crypto_list))
+    crypto_array = pd.DataFrame(index=np.arange(0, discrete_points),
+                                columns=(i for i in crypto_list))
+
+    #update dataframe, sleep 60 minute intervals.
     update_dataframe(crypto_array, crypto_list, discrete_points, market)
 
     # output dataframe to a csv for later analysis
