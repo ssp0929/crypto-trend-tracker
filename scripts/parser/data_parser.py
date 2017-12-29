cf60f7e09ef6ccef1ed1826080b3452b05513d2d
Stephen Park <ssp0929@gmail.com>
2017-12-29 16:14:16 -0600

    data parser skeleton slightly more hashed out

--- scripts/parser/data_parser.py
+++ scripts/parser/data_parser.py
@@ -1,5 +1,28 @@
+'''Parse reddit and twitter exported JSON data to track mention metrics'''
 import json
+from pprint import pprint
 
-# load JSON files
-reddit_data = json.load(open('data/reddit_data.json'))
-# TODO: twitter_data = json.load(open('data/twitter_data.json'))
+def reddit_parse():
+
+    '''Parse reddit data'''
+
+    reddit_data = json.load(open('data/reddit_data.json'))
+    cryptoscore_list = json.load(open('cryptocurrencies/cryptoscore.json'))
+
+    # Iterate through data and track mentions
+
+def twitter_parse():
+
+    '''Parse twitter data'''
+
+    twitter_data = json.load(open('data/twitter_data.json'))
+
+def main():
+
+    ''' program driver '''
+
+    reddit_parse()
+    # twitter_parse()
+
+if __name__ == '__main__':
+    main()
