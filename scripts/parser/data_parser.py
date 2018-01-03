0cf6db522ca0b14a0e480dc3c9b285c0d117330a
Stephen Park <ssp0929@gmail.com>
2018-01-02 19:18:09 -0600

    Fixed incorrect file name for load.

--- scripts/parser/data_parser.py
+++ scripts/parser/data_parser.py
@@ -6,7 +6,7 @@ def reddit_parse():
 
     '''Parse reddit data'''
 
-    reddit_data = json.load(open('data/reddit_data.json'))
+    reddit_data = json.load(open('data/reddit_data_score.json'))
     cryptoscore_list = json.load(open('cryptocurrencies/cryptoscore.json'))
 
     # Iterate through data and track mentions
