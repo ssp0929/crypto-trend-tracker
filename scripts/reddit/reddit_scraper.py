b1381bc7cef06aae1ef4745b7ebb79fdff9e38c3
Stephen Park <ssp0929@gmail.com>
2018-01-12 14:25:43 -0600

    Reddit scraper is pretty much finished. Have to test if comment filtering after time since last run works.

--- scripts/reddit/reddit_scraper.py
+++ scripts/reddit/reddit_scraper.py
@@ -5,6 +5,7 @@ Reddit bot to scrape reddit and export submission_object and comment data for la
 
 import logging
 import json
+import time
 import praw
 
 def log_init():
@@ -17,21 +18,36 @@ def log_init():
     logger.setLevel(logging.DEBUG)
     logger.addHandler(handler)
 
-def scrape():
+def create_comment_object(comment, matches_list):
+
+    ''' Create comment object for inclusion in context file '''
+
+    comment_object = {}
+    comment_object['score'] = comment.score
+    comment_object['matches'] = matches_list
+    comment_object['content'] = comment.body
+
+    return comment_object
+
+def create_submission_object(submission, comment_list):
+
+    ''' Create submission object for inclusion in context file'''
+
+    submission_object = {}
+    submission_object['title'] = submission.title
+    submission_object['score'] = submission.score
+    submission_object['comments'] = comment_list
+
+    return submission_object
+
+def scrape(reddit):
 
     '''
     Initialize praw Reddit instance.
     This bot only scrapes data, so read-only is fine.
     '''
 
-    # Load from credentials file that isn't tracked by git.
-    credentials = json.load(open('credentials.json')).get('reddit')
-    reddit = praw.Reddit(client_id=credentials['client_id'],
-                         client_secret=credentials['client_secret'],
-                         user_agent=credentials['user_agent'],
-                         username=credentials['username'])
-
-    # Final array that will be written to score outfile.
+    # Final array that will be written to reddit_output outfile.
     data_score = []
 
     # Multi-word / hyphenated crypto IDs will be stripped to first word.
@@ -51,75 +67,88 @@ def scrape():
             cryptosymbols_list.append(line.strip())
 
     # Subreddits to scrape
-    # TODO: Clean up this method. Big O(n*m*k*l) not good.
-    # Relatively inconsequential for a script that only runs once every 60 minutes...
-    subreddits_to_track = ['cryptocurrency']
+    # Subpar Big(O) but...
+    # relatively inconsequential for a script that only runs once every 60 minutes...
+    with open('reddit_config.json', 'r') as readfile:
+        reddit_config = json.load(readfile)
+        subreddits_to_track = reddit_config.get('subreddits-to-track')
+        last_run_time = reddit_config.get('last-run-time', 0)
+        reddit_config.update({ 'last-run-time': time.time() })
+
+    with open('reddit_config.json', 'w') as outfile:
+        json.dump(reddit_config, outfile)
+
     for tracked_subreddit in subreddits_to_track:
         subreddit = reddit.subreddit(tracked_subreddit)
 
-        # Final array that will be written to score_context outfile.
+        # Final array that will be written to context data outfile.
         data = []
 
         # Iterate through a subreddit's top X submissions
         for submission in subreddit.hot(limit=25):
-            submission_object = {}
+            # List of comments tied to a specific submission thread
             comment_list = []
 
             # Iterate through comment tree.
             submission.comments.replace_more(limit=0)
             for comment in submission.comments:
-                comment_object = {}
-
                 # Filter for comment scores that are less than a certain score
                 if comment.score < 2:
                     continue
 
+                # Filter for comments made after last check date
+                if comment.created < last_run_time:
+                    continue
+
                 # Check to see if any in cryptolist/cryptotickers are mentioned
                 # Check if any cryptos were found, if none then discard comment from context.
-                match_found = False
+                match_not_found = True
                 # List of matches associated with a comment block, used for context
                 matches_list = []
-                for i in range(len(cryptolist_list)):
+                for i in range(0, len(cryptolist_list)):
                     row = data_score[i]
                     comment_contents = comment.body.lower().split()
 
-                    # Find matches
+                    # Find matches and increment score
                     if cryptolist_list[i] in comment_contents or \
                        cryptosymbols_list[i] in comment_contents:
                         matches_list.append(cryptolist_list[i])
-                        match_found = True
+                        match_not_found = False
                         row[1] = row[1] + 1
 
                 # If match not found, then don't include comment in context data.
-                if not match_found:
+                if match_not_found:
                     continue
                 else:
-                    comment_object['score'] = comment.score
-                    comment_object['matches'] = matches_list
-                    comment_object['content'] = comment.body
-
                     # Append to a list for later inclusion into submission_object
-                    comment_list.append(comment_object)
+                    comment_list.append(create_comment_object(comment, matches_list))
 
             # Create submission_object mapped to a single submission
-            submission_object['title'] = submission.title
-            submission_object['score'] = submission.score
-            submission_object['comments'] = comment_list
-            data.append(submission_object)
+            data.append(create_submission_object(submission, comment_list))
 
         # Export to JSON file. This contains some context to score data.
-        with open('data/reddit_data.json', 'w') as outfile:
+        with open('scripts/reddit/context_data.json', 'w') as outfile:
             json.dump(data, outfile)
 
         # Export to JSON file. This contains calculated score totals.
-        with open('data/reddit_data_score.json', 'w') as outfile:
+        with open('data/reddit_output.json', 'w') as outfile:
             json.dump(data_score, outfile)
 
 def main():
 
     ''' Driver for program '''
+
+    # Load from credentials file that isn't tracked by git.
+    with open('credentials.json', 'r') as readfile:
+        credentials = json.load(readfile).get('reddit')
+
+    reddit = praw.Reddit(client_id=credentials.get('client_id'),
+                         client_secret=credentials.get('client_secret'),
+                         user_agent=credentials.get('user_agent'),
+                         username=credentials.get('username'))
+
     log_init()
-    scrape()
+    scrape(reddit)
 
 if __name__ == '__main__':
     main()
