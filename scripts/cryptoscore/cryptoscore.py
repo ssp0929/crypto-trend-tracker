5f2df6f4678705826a85d6f7ed29c8c9b7d84a00
Stephen Park <ssp0929@gmail.com>
2018-01-12 14:36:05 -0600

    Updated cryptoscore template generator, generated a sample file of 3 hourly runs in 3 hours.

--- scripts/cryptoscore/cryptoscore.py
+++ scripts/cryptoscore/cryptoscore.py
@@ -22,7 +22,7 @@ def generate_cryptoscore(current_cryptolist, curated_cryptolist):
     t_score = []
 
     # 720 points corresponding to hourly scrape data.
-    for i in range(720):
+    for i in range(3):
         t_score.append(new_cryptolist)
 
     # Export to JSON
