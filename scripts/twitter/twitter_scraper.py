c8c19c754f786e42fa15da619c1acbe885026d8c
Stephen Park <ssp0929@gmail.com>
2018-01-12 19:20:44 -0600

    Fixed twitter, might need to make the crypto list a LOT bigger

--- scripts/twitter/twitter_scraper.py
+++ scripts/twitter/twitter_scraper.py
@@ -8,7 +8,7 @@ import io
 import tweepy
 from tweepy import OAuthHandler
 
-def create_status_object(status):
+def create_status_object(status, matches_list):
 
     ''' Create status object to append to list of tweets '''
 
@@ -18,6 +18,7 @@ def create_status_object(status):
     status_object['twitter_handle'] = status.get('user').get('screen_name')
     status_object['favorites'] = status.get('favorite_count')
     status_object['retweets'] = status.get('retweet_count')
+    status_object['matches'] = matches_list
     status_object['text'] = status.get('full_text', status.get('text'))
 
     return status_object
@@ -33,11 +34,29 @@ def get_list_timeline(api):
         list_owner = twitter_config.get('list-owner')
         last_run_id = twitter_config.get('last-run-id', '1')
 
-    # Get timeline of tweets/retweets to terminal
-    # include_rts = False refers to reteweets being included or not
-    list_timeline = api.list_timeline(list_owner, list_name, since_id=last_run_id, page=5)
+
+
+    # Multi-word / hyphenated crypto IDs will be stripped to first word.
+    # Also populate score array with nonstripped crypto IDs.
+    # ASSUMPTION: Cryptolist and Cryptotickers are sorted for a 1 to 1 mapping.
+    # Convert files to list
+    cryptolist_list = []
+    cryptosymbols_list = []
     data = []
     data_score = []
+
+    with open('input_data/cryptolist.txt', 'r') as readfile:
+        for line in readfile:
+            data_score.append([line.strip(), 0])
+            cryptolist_list.append(line.split("-")[0].strip())
+
+    with open('input_data/cryptotickers.txt', 'r') as readfile:
+        for line in readfile:
+            cryptosymbols_list.append(line.strip())
+
+    # Get timeline of tweets/retweets to terminal
+    # include_rts = False refers to reteweets being included or not
+    list_timeline = tweepy.Cursor(api.list_timeline, list_owner, list_name, since_id = last_run_id).items(200)
     last_run_id_not_logged = True
 
     for status in list_timeline:
@@ -52,10 +71,38 @@ def get_list_timeline(api):
 
         # If tweet truncated then re-search using extended mode
         if status.truncated:
-            extended_status = api.get_status(status.id_str, tweet_mode='extended')
-            data.append(create_status_object(extended_status))
+            status = api.get_status(status.id_str, tweet_mode='extended')
+
+        # Check to see if any in cryptolist/cryptotickers are mentioned
+        # Check if any cryptos were found, if none then discard comment from context.
+        match_not_found = True
+
+        # If full_text exists grab it, otherwise use text
+        tweet_contents = ''
+        try:
+            status.full_text
+        except AttributeError:
+            tweet_contents = status.text.lower().split()
         else:
-            data.append(create_status_object(status))
+            tweet_contents = status.full_text.lower().split()
+
+        # List of matches associated with a comment block, used for context
+        matches_list = []
+        for i in range(0, len(cryptolist_list)):
+            row = data_score[i]
+            # Find matches and increment score
+            if cryptolist_list[i] in tweet_contents or \
+                cryptosymbols_list[i] in tweet_contents:
+                matches_list.append(cryptolist_list[i])
+                match_not_found = False
+                row[1] = row[1] + 1
+
+        # If match not found, then don't include comment in context data.
+        if match_not_found:
+            continue
+        else:
+            # Append to a list for later inclusion into submission_object
+            data.append(create_status_object(status, matches_list))
 
     # Log last_run_id to twitter_config and write out
     with open('twitter_config.json', 'w') as outfile:
