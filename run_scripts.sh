015b2a282d64c70dce2d11bf036f500948e7e768
Stephen Park <ssp0929@gmail.com>
2018-01-30 17:23:14 -0600

    Works on my pi, strange...

--- run_scripts.sh
+++ run_scripts.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 echo "Generating Cryptoscore Template"
-/usr/local/bin/pipenv run python scripts/cryptoscore/cryptoscore.py
+python3 scripts/cryptoscore/cryptoscore.py
 echo "Running Reddit Scraper"
-/usr/local/bin/pipenv run python scripts/reddit/reddit_scraper.py
+python3 scripts/reddit/reddit_scraper.py
 echo "Running Twitter Scraper"
-/usr/local/bin/pipenv run python scripts/twitter/twitter_scraper.py
+python3 scripts/twitter/twitter_scraper.py
 echo "Running Coinmarketcap Scraper"
-/usr/local/bin/pipenv run python scripts/coinmarketcap/cmc_scraper.py
+python3 scripts/coinmarketcap/cmc_scraper.py
 echo "Running Data Parser"
-/usr/local/bin/pipenv run python scripts/parser/data_parser.py
+python3 scripts/parser/data_parser.py
 echo "Generating daily snapshot"
-/usr/local/bin/pipenv run python scripts/snapshot/snapshot_script.py
+python3 scripts/snapshot/snapshot_script.py
 echo "Sleeping for 30 seconds... check for errors."
 sleep 30s
